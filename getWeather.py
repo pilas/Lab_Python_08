e78a04bf145c949b751ef70e48699ee8065717b9
pilas <pilas@localhost>
2012-07-04 08:52:43 +0000

    weather

--- getWeather.py
+++ getWeather.py
@@ -1,8 +1,26 @@
 import urllib
+import urllib.request
 import re
-
+'''
 # Get a file-like object for the Python Web site's home page.
 f = urllib.urlopen("http://www.wunderground.com/global/stations/65432.html")
 # Read from the object, storing the page's contents in 's'.
 s = f.read()
 f.close()
+
+
+import webbrowser
+webbrowser.open("http://www.wunderground.com/global/stations/65432.html")
+f = urllib2.urlopen("http://www.wunderground.com/global/stations/65432.html")
+s = f.read()
+f.close()
+.decode('utf8')  °C\s+'
+'''
+f = urllib.request.urlopen("http://www.wunderground.com/global/stations/65432.html")
+s = f.read().decode('utf8')
+f.close()
+
+print( re.findall(r'(\d+(|\.)\d+)\&deg',s))
+#print(s)
+
+
